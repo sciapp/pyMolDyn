e9f670250bdcd7723cfc90d979cce74203a19bd5
David Knodt <d.knodt@fz-juelich.de>
2014-08-28 14:56:42 +0200

    specification generator for config object
    file to object parser finished but untested, object to file parser in
    development

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,3 +1,20 @@
+import configobj
+import validate
+import os.path
+import inspect
+
+CONFIG_FILE = 'config.cfg'
+CONFIG_SPEC_FILE = 'config.spec'
+
+# second string is the list type name
+type_dict = {
+    int: ('integer', 'int'),
+    float: ('float', 'float'),
+    str: ('string', 'string'),
+    bool: ('boolean', 'bool')
+}
+
+
 class Configuration:
 
     # standard configuration
@@ -5,7 +22,6 @@ class Configuration:
     GL_WINDOW_SIZE  = (800, 800)
     WINDOW_POSITION = (-1, -1)
     STD_RESOLUTION  = 64
-
 
     class Colors:
         CAVITY          = (0.2, 0.4, 1)
@@ -20,4 +36,55 @@ class Configuration:
         ATOM_RADIUS     = 2.8
 
     def __init__(self):
-        pass
+        self.file = ConfigFile(self)
+
+    def save(self):
+        self.file.write()
+
+    def read(self):
+        self.file.read()
+
+class ConfigFile:
+
+    def __init__(self, config):
+        self.config = config
+        self.read()
+
+    def write(self):
+        self.file.write()
+
+    def generate_configspec(self):
+        spec_file = configobj.ConfigObj(CONFIG_SPEC_FILE)
+        self.generate_spec_for_section(self.file, spec_file)
+        spec_file.write()
+
+    def generate_spec_for_section(self, section, spec_section):
+        for scalar in section.scalars:
+            t = type(section[scalar])
+            type_string = type_dict[t][0] if t is not list else type_dict[type(section[scalar][0])][1]+'_list'
+            spec_section[scalar] = type_string
+        for sect in section.sections:
+            spec_section[sect] = {}
+            self.generate_spec_for_section(section[sect], spec_section[sect])
+
+    def obj2file(self):
+        self.file = configobj.ConfigObj(CONFIG_FILE)
+        for attr in dir(self.config):
+            if inspect.isclass(attr):
+                #TODO incomplete
+                pass
+
+
+    def read(self):
+        if not os.path.isfile(CONFIG_SPEC_FILE):
+            pass                                        #TODO in case spec file does not exist
+        validator = validate.Validator()
+        self.file = configobj.ConfigObj(CONFIG_FILE, configspec=CONFIG_SPEC_FILE)
+        self.file.validate(validator)
+        self.parse_section(self.file, self.config)
+
+    def parse_section(self, section, config_obj):
+        for scalar in section.scalars:
+            setattr(config_obj, scalar, section[scalar])
+        for sec in section.sections:
+            self.parse_section(sec, getattr(self.config, sec))
