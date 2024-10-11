c48320d7d971569cb75dbe8958d5a2ec8c4220b2
Ingo Meyer <i.meyer@fz-juelich.de>
2024-10-11 16:40:53 +0200

    Fix Windows compatibility of the C extensions

--- pymoldyn/core/calculation/extension/algorithm.c
+++ pymoldyn/core/calculation/extension/algorithm.c
@@ -4,6 +4,12 @@
 #include <limits.h>
 #include <gr3.h>
 
+#ifdef _WIN32
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
 #define SQUARE(x) ((x)*(x))
 #define CLIP(x,a,b) ((x)<(a)?(a):((x)>(b)?(b):(x)))
 
@@ -38,7 +44,7 @@ typedef struct subgrid {
  * check if this atom is the closest to this cell
  * and write the atom index (+1) into it.
  */
-void atomstogrid(
+EXPORT void atomstogrid(
         int64_t *grid, int dimensions[3], int strides[3],
         int natoms, int *atom_positions, int *radii_indices,
         int nradii, int *radii,
@@ -140,7 +146,7 @@ void atomstogrid(
  * Routines to work with subgrids
  */
 
-subgrid_t *subgrid_create(int cubesize, int grid_dimensions[3])
+EXPORT subgrid_t *subgrid_create(int cubesize, int grid_dimensions[3])
 {
     subgrid_t *sg;
     int k;
@@ -160,7 +166,7 @@ subgrid_t *subgrid_create(int cubesize, int grid_dimensions[3])
     return sg;
 }
 
-void subgrid_destroy(subgrid_t *sg)
+EXPORT void subgrid_destroy(subgrid_t *sg)
 {
     int i;
 
@@ -189,7 +195,7 @@ int subgrid_index(subgrid_t *sg, int *pos)
     return index;
 }
 
-void subgrid_add_atoms(subgrid_t *sg,
+EXPORT void subgrid_add_atoms(subgrid_t *sg,
         int natoms, int *atom_positions,
         int ntranslations, int *translations)
 {
@@ -213,7 +219,7 @@ void subgrid_add_atoms(subgrid_t *sg,
     }
 }
 
-void subgrid_add_domains(subgrid_t *sg,
+EXPORT void subgrid_add_domains(subgrid_t *sg,
         int npoints, int *domain_indices, int *domain_points,
         int ntranslations, int *translations)
 {
@@ -248,7 +254,7 @@ void subgrid_add_domains(subgrid_t *sg,
  * For each cell, determine if it is closer to a cavity domain than
  * to an atom center. If so, mark the cell in the grid.
  */
-void mark_cavities(int64_t *grid, int64_t *domain_grid, int dimensions[3], int strides[3],
+EXPORT void mark_cavities(int64_t *grid, int64_t *domain_grid, int dimensions[3], int strides[3],
         char *discretization_grid, int discgrid_strides[3],
         subgrid_t *sg, int use_surface_points)
 {
@@ -350,7 +356,7 @@ void mark_cavities(int64_t *grid, int64_t *domain_grid, int dimensions[3], int s
 
 #define INDEXGRID(i,j,k) ((int64_t)(i)*strides[0]+(j)*strides[1]+(k)*strides[2])
 #define INDEXDISCGRID(i,j,k) ((int64_t)(i)*discgrid_strides[0]+(j)*discgrid_strides[1]+(k)*discgrid_strides[2])
-int cavity_triangles(
+EXPORT int cavity_triangles(
         int64_t *cavity_grid,
         int dimensions[3],
         int strides[3],
@@ -492,14 +498,14 @@ int cavity_triangles(
 #undef INDEXDISCGRID
 
 
-void free_float_p(float *p)
+EXPORT void free_float_p(float *p)
 {
     free(p);
 }
 
 
 #define INDEXGRID(i,j,k) ((int64_t)(i)*strides[0]+(j)*strides[1]+(k)*strides[2])
-void cavity_intersections(
+EXPORT void cavity_intersections(
         int64_t *grid,
         int dimensions[3],
         int strides[3],
@@ -548,7 +554,7 @@ void cavity_intersections(
  * inside has an equivalent (i.e. reachable through a translation vector)
  * cell inside; and that every cell outside has an equivalent cell inside.
  */
-void mark_translation_vectors(int8_t *grid,
+EXPORT void mark_translation_vectors(int8_t *grid,
                               int dimensions[3],
                               int strides[3],
                               int ntranslations,
